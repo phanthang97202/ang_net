e5e1e0a737b7452af653a4df1adc8d64b7d058fd
phanthang97202 <phanthang97202@gmail.com>
2024-10-30 17:11:16 +0700

    edit sql file

--- script_db.sql
+++ script_db.sql
@@ -23,3 +23,38 @@ insert into News values
 update News  
 set ContentBody = '"<h3><strong style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">Quirk và kh? n?ng ??c bi?t c?a Toga Himiko</strong></h3><ul><li><strong style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">Quirk: Transform:</strong><span style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\"> Kh? n?ng bi?n ??i ngo?i hình và máu thành c?a ng??i khác b?ng cách u?ng máu c?a h?.</span></li><li><strong style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">?u ?i?m:</strong><span style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\"> Linh ho?t trong chi?n ??u, kh? n?ng ng?y trang hoàn h?o, có th? s? d?ng s?c m?nh c?a nh?ng ng??i mà cô ?ã bi?n ??i.</span></li><li><strong style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">H?n ch?:</strong><span style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\"> C?n ph?i u?ng máu th??ng xuyên ?? duy trì bi?n ??i, có th? b? m?t ki?m soát khi u?ng quá nhi?u máu.</span></li></ul><p></p><p></p><p>Tác gi?: PHan Thang</p>"' 
 where NewsId = 'toga'
+
+select * from News
+select * from HashTagNews
+select * from RefFileNews
+select * from PointNews
+
+delete from News
+delete from HashTagNews
+
+insert into PointNews values 
+('stain-hero-killer', '99841345-efbb-4b47-ad3c-cb60ad73e4e0', '2024-10-10 23:30:52.0811634', 1, 8.9, '2024-10-10 23:30:52.0811634')
+insert into PointNews values 
+('stain-hero-killer', '502871db-e932-4894-8d74-69beb5aecd73', '2024-10-10 23:30:52.0811634', 1, 8, '2024-10-10 23:30:52.0811634')
+
+select n.NewsId, r.FileUrl, h.HashTagNewsName, p.Point, Sum(p.Point)  as AvgPoint 
+from News n
+left join RefFileNews r on n.NewsId = r.NewsId  
+left join HashTagNews h on n.NewsId = h.NewsId
+left join PointNews p on n.NewsId = p.NewsId
+group by n.NewsId, r.FileUrl, h.HashTagNewsName, p.Point
+having n.NewsId = 'stain-hero-killer'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
